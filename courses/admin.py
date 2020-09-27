fa95a2c6c5d3318977ef28fb2abe5ed720809b9b
meritus freedy <meritusfreedy@gmail.com>
2020-09-26 20:22:43 -0400

    remake models

--- courses/admin.py
+++ courses/admin.py
@@ -1,23 +1,32 @@
 from django.contrib import admin
-from .models import Courses,Attendee
+from .models import Courses,Attendee,Date
 from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
 # Register your models here.
 class CoursesAdmin(admin.ModelAdmin,DynamicArrayMixin):
-    list_display = ('id','title','teacher','price','date',)
+    list_display = ('id','title','teacher','price',)
     list_display_links = ('id',)
     list_filter = ('teacher__username',)
     search_fields = ('title','description',)
     list_per_page = 25
     autocomplete_fields = ['teacher','requirements']
 
+class DateAdmin(admin.ModelAdmin):
+    list_display = ('id','date','course','nb_attendees',)
+    list_display_links = ('id',)
+    list_filter = ('date','nb_attendees',)
+    search_fields = ('nb_attendees','date',)
+    list_per_page = 25
+    autocomplete_fields = ['course','attendees']
+
 class AttendeeAdmin(admin.ModelAdmin):
-    list_display = ('id','course','attendee','score','complete',)
+    list_display = ('id','date','attendee','score','complete',)
     list_display_links = ('id',)
     list_filter = ('score','complete')
-    search_fields = ('attendee__username','course__title',)
+    search_fields = ('attendee__username','date__course__title',)
     list_per_page = 25
-    autocomplete_fields = ['course','attendee']
+    autocomplete_fields = ['date','attendee']
 
 admin.site.register(Attendee, AttendeeAdmin)
 admin.site.register(Courses, CoursesAdmin)
+admin.site.register(Date, DateAdmin)
