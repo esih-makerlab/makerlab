5481bc30c8005acd2aa0c4441083a208c8d06d5c
Madsen Servius <madsen@madsendynamics.com>
2020-09-27 02:10:33 -0400

    add profile user

--- member/models.py
+++ member/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Profile(models.Model):
+    """
+    A User Profile model. Complementary data of 'Django auth models'
+    """
+
+    class Meta:
+        verbose_name_plural = 'Profiles'
+
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE
+    )
+
+    birthdate = models.DateField('Birthdate')
+    
+    biography = models.TextField('Biography', blank=True)
+    
