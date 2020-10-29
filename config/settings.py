60e26fb38cdbd8a164db25f0d5ca4ceff050e052
meritus freedy <meritusfreedy@gmail.com>
2020-10-29 18:54:15 -0400

    remove allauth

--- config/settings.py
+++ config/settings.py
@@ -164,10 +164,7 @@ MEDIA_URL = "/media/"
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
-    # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
+    'django.contrib.auth.backends.ModelBackend'
 ]
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
