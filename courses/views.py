f59fae7ff12ce9c7e6ff57229d91dd0bea927e67
meritus freedy <meritusfreedy@gmail.com>
2020-11-05 12:48:40 -0500

    init search

--- courses/views.py
+++ courses/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils import timezone
 
-from .models import Course
+from .models import Course,CourseDate
 
 def home(request):
         
@@ -27,3 +27,27 @@ def course_enrollement(request):
 
 def course_payement(request):
     return render(request, 'courses/payement.html')
+
+def search_results(request):
+    q = request.GET.get('q')
+
+    filter_search = request.GET.get('filter')
+
+    if not filter_search:
+        filter_search = 'course'
+
+    if filter_search == 'teacher':
+        teachers = [
+
+        ]
+    else:
+
+
+        courses = [
+            {'title':'intro C++','description':'C++ est un langage de programmation compilé permettant la programmation sous de multiples paradigmes. Ses bonnes performances, et sa compatibilité avec le C en font un des langages de programmation les plus utilisés dans les applications où la performance est critique.'},
+            {'title':'intro C++','description':'C++ est un langage de programmation compilé permettant la programmation sous de multiples paradigmes. Ses bonnes performances, et sa compatibilité avec le C en font un des langages de programmation les plus utilisés dans les applications où la performance est critique.'},
+            {'title':'intro C++','description':'C++ est un langage de programmation compilé permettant la programmation sous de multiples paradigmes. Ses bonnes performances, et sa compatibilité avec le C en font un des langages de programmation les plus utilisés dans les applications où la performance est critique.'},
+            {'title':'intro C++','description':'C++ est un langage de programmation compilé permettant la programmation sous de multiples paradigmes. Ses bonnes performances, et sa compatibilité avec le C en font un des langages de programmation les plus utilisés dans les applications où la performance est critique.'}
+        ]
+
+    return render(request, 'courses/search_results.html',{'courses':courses,'teachers':teachers,'filter':filter_search})
