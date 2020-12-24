fb876228e284caf671eca080484f2b845562bbe6
meritus freedy <meritusfreedy@gmail.com>
2020-12-23 20:48:07 -0500

    bug correction requirements

--- config/settings.py
+++ config/settings.py
@@ -226,7 +226,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "staticfiles")
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
