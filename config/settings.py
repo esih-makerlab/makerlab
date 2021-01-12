5a456359178cfa4e0cca6d282d1cebbb7d1cc276
Erico Saint-Charles <68854342+youko509@users.noreply.github.com>
2021-01-12 11:08:08 -0500

    update static dirs

--- config/settings.py
+++ config/settings.py
@@ -240,7 +240,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles")
+    os.path.join(BASE_DIR, "static")
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
