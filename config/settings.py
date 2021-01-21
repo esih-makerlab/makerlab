7805f4f4fe2164d2b703db25a3b54f840d60603c
meritus freedy <meritusfreedy@gmail.com>
2021-01-21 13:38:44 -0500

    add makerlab.cleverapps.io to allowedhost

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','makerlab.cleverapps.io']
 
 
 # Application definition
